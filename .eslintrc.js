bc5ad11e31f24c6e21dfe2a66b82542c28ed830d
Scott Cooper <scttcper@gmail.com>
2020-02-08 19:40:09 -0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['xo-space/esnext', 'xo-typescript'],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-explicit-any': 0,
+  },
+};
