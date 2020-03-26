aff811670048797b05081bc1b7d743b5bc8b5f6e
Scott Cooper <scttcper@gmail.com>
2020-03-26 15:42:31 -0700

    wait for media to sync

--- test/add-media.ts
+++ test/add-media.ts
@@ -1,10 +1,13 @@
 import { createClient } from './test-client';
 
+const delay = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export async function addMedia(): Promise<void> {
   const server = await createClient();
   const library = await server.library();
   await library.add('Movies', 'movie', 'com.plexapp.agents.imdb', 'Plex Movie Scanner', '/data/movies');
   await library.add('TV Shows', 'show', 'com.plexapp.agents.thetvdb', 'Plex Series Scanner', '/data/shows');
+  await delay(10000);
 }
 
 if (!module.parent) {
