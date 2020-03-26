f30d641c86d583cc6f618b89b929e028dbd288b4
Scott Cooper <scttcper@gmail.com>
2020-03-26 15:43:26 -0700

    expect length 2

--- test/library.spec.ts
+++ test/library.spec.ts
@@ -5,6 +5,7 @@ describe('Library', () => {
     const server = await createClient();
     const library = await server.library();
     const sections = await library.sections();
+    expect(sections).toHaveLength(2);
     expect(sections[0].type).toBe('movie');
     expect(sections[1].type).toBe('show');
   });
