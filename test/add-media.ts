01a2ae6ea256c3391f1c996a1f31dfe48fcdd33e
Scott Cooper <scttcper@gmail.com>
2020-03-27 20:08:51 -0700

    wait longer after claim

--- test/add-media.ts
+++ test/add-media.ts
@@ -3,7 +3,7 @@ import { createClient } from './test-client';
 const delay = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export async function addMedia(): Promise<void> {
-  await delay(8000);
+  await delay(10000);
   const server = await createClient();
   const library = await server.library();
   await library.add('Movies', 'movie', 'com.plexapp.agents.imdb', 'Plex Movie Scanner', '/data/movies');
