53bd4491eebb375d7caef8afc2b0d3109e7f3c23
Scott Cooper <scttcper@gmail.com>
2024-04-21 19:24:48 -0700

    switch to pnpm

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    threads: false,
     coverage: {
+      provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
   },
